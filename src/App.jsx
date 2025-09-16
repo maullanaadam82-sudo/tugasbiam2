261d1de15d8dc0cfaa31a771b43e2cce446960a4
qyn <qyn@example.com>
2025-09-16 14:28:53 +0700

    Fix line endings App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./index.css";   
 
 export default function App() {
   const [username, setUsername] = useState("");
